05e3f0e3c97bc19f58009bda8c00b79122e58956
Francisco Lairton Rodolfo Calaca (ALLAN) <75245185+allanrodz@users.noreply.github.com>
2024-02-15 17:47:41 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ document.body.addEventListener('click', function() {
     }, 10); // Slight delay to ensure CSS transition takes effect
 });
 
+document.body.addEventListener('click', function() {
+    var message = document.getElementById('click-message');
+    if (message) {
+        message.style.display = 'none'; // Hide the message
+    }
+    // You can also add your code here to show the overlay if it's part of the interaction
+});
+
+
 function hideOverlay() {
     document.getElementById('overlay').style.display = 'none';
     document.querySelector('.table-container').style.opacity = 0;
